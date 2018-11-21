dc27df96f3947eaa15abf3286b5deb79e6962ebb
andrgolubev <andpgolubev@gmail.com>
2018-11-21 21:07:40 +0300

    Initial version of build/test script

---
+++ script/test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import sys
+import subprocess
+import shutil
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('Command-line options for build script')
+    parser.add_argument('--debug', action='store_true', default=False,
+        help='Specify whether debug build should be use')
+    parser.add_argument('--cmake', default='', nargs='*',
+        help='Pass raw arguments to cmake')
+    return parser.parse_args()
+
+
+def run_steps(steps, work_dir='.'):
+    """Run \n separated commands"""
+    for step in steps.splitlines():
+        step = step.strip()
+        if not step:
+            continue
+        sys.stdout.write('[{name}]\n'.format(name=step))
+        sys.stdout.flush()
+        subprocess.check_call(step, shell=True, cwd=work_dir)
+        sys.stdout.flush()
+
+
+def test(debug, cmake_args, work_dir='.'):
+    t = 'Debug' if debug else 'Release'
+    build_type = '-DCMAKE_BUILD_TYPE={t}'.format(t=t)
+    steps = """
+    cmake {build_type} {args} ..
+    cmake --build .
+    ./vrp_solver ../test_data/sample_input.csv
+    """.format(build_type=build_type, args=cmake_args)
+    run_steps(steps, work_dir=work_dir)
+
+
+def main():
+    """Main entrypoint"""
+    args = parse_args()
+    workdir = os.path.join(os.path.dirname(__file__), '..', 'build')
+    if os.path.exists(workdir):
+        shutil.rmtree(workdir)
+    os.makedirs(workdir)
+    cmake_args = args.cmake
+    for i, arg in enumerate(cmake_args):
+        if not arg.startswith('-D'):
+            cmake_args[i] = '-D' + arg
+    test(args.debug, cmake_args, workdir)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
